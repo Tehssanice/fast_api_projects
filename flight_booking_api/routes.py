bd734b898276a10996cfdda7c4c018fe0e17c7ef
Nwaru Chidubem Theresa <tessychidubem@gmail.com>
2024-04-02 10:53:59 +0100

    flight_booking_api update

--- flight_booking_api/routes.py
+++ flight_booking_api/routes.py
@@ -1,13 +1,14 @@
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
-from models import Booking, Responses
+from models import Booking
+from models import Responses
 from services import process_booking
 
 
 booking_router = APIRouter(prefix="/booking", tags=["users"])
 
 
-@booking_router.post("/", response_model=Responses)
+@booking_router.post("/", responses=Responses)
 async def create_booking(booking: Booking):
     results = await process_booking(booking)
     return JSONResponse(status_code=201, content={"data": results, "message": "Booking successful"})
