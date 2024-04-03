d7237a9c26b5745ca9a540772164b0a5e49de301
Nwaru Chidubem Theresa <tessychidubem@gmail.com>
2024-04-03 13:17:21 +0100

    Update main.py

--- product_search_api/main.py
+++ product_search_api/main.py
@@ -1,18 +1,6 @@
-from fastapi import FastAPI, Query
-from typing import Optional, List
-from models import Product, ProductSearchQuery
-from services import search_products
+from fastapi import FastAPI
+from routes import router as product_router
 
 app = FastAPI()
 
-
-@app.get("/products/search", response_model=List[Product])
-async def search_product(
-    name: Optional[str] = Query(None, min_length=3, max_length=50),
-    category: Optional[str] = Query(None, min_length=3, max_length=50),
-    min_price: Optional[float] = Query(None, ge=0),
-    max_price: Optional[float] = Query(None, ge=0)
-):
-    query = ProductSearchQuery(name=name, category=category,
-                               min_price=min_price, max_price=max_price)
-    return search_products(query)
+app.include_router(product_router, prefix="/products", tags=["products"])
